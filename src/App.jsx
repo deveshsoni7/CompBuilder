579d45d456b85597b10558b660f0d6568923cea2
deveshsoni7 <devesh.soniwork@gmail.com>
2025-09-28 01:11:24 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import './App.css'
+import Home from './pages/Home';
+import NoPage from './pages/NoPage';
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        
+
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="*" element={<NoPage/>} />
+          
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
